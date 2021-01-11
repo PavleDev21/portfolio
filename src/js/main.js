b57db711906152351127bcac42d93904baa31481
PavleDev21 <pavle.barackov@gmail.com>
2021-01-11 20:57:58 +0100

    Banner finished and about section started

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,16 @@ $nav = $('.navigation');
 $navBtn.on('click', function() {
     $nav.toggleClass('nav-show');
 })
+
+$('.banner-subtitle').delay(1500).animate({opacity: 1}, {duration: 700});
+
+var $aboutSticky = $('.about-card');
+var sectionOffsetTop = $('.about').offset().top;
+
+/*$(document).scroll(function() {
+    if ( $(document).scrollTop() >= sectionOffsetTop ) {
+        $aboutSticky.addClass('about-fixed');
+    } else if ( $(document).scrollTop() < sectionOffsetTop ){
+        $aboutSticky.removeClass('about-fixed');
+    }
+});*/
