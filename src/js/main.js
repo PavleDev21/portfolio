4750429deec0625475f84ab913937aaf3aade08d
PavleDev21 <pavle.barackov@gmail.com>
2021-01-12 00:25:45 +0100

    Fixed function for sticky about

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,20 @@ $('.banner-subtitle').delay(1500).animate({opacity: 1}, {duration: 700});
 
 var $aboutSticky = $('.about-card');
 var sectionOffsetTop = $('.about').offset().top;
+var triggerOffset = sectionOffsetTop + $('.about').height() - $aboutSticky.height();
+console.log(triggerOffset);
 
-/*$(document).scroll(function() {
+$(document).scroll(function() {
     if ( $(document).scrollTop() >= sectionOffsetTop ) {
         $aboutSticky.addClass('about-fixed');
-    } else if ( $(document).scrollTop() < sectionOffsetTop ){
+    } else if ( $(document).scrollTop() < sectionOffsetTop){
         $aboutSticky.removeClass('about-fixed');
     }
-});*/
+
+    if ( $(document).scrollTop() > triggerOffset ) {
+        $aboutSticky.removeClass('about-fixed');
+        $aboutSticky.addClass('about-bottom');
+    } else if( $(document).scrollTop() <= triggerOffset ) {
+        $aboutSticky.removeClass('about-bottom');
+    }
+});
